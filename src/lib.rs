5174017085745231c8b11885c5e85daae256c665
Haydn Vestal <haydnv@gmail.com>
2021-01-05 17:11:48 -0600

    add Range struct & bisect by range method

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,34 @@
+use std::borrow::Borrow;
 use std::cmp::Ordering;
 use std::marker::PhantomData;
-use std::ops::Deref;
+use std::ops::{Bound, Deref};
+
+mod range;
+
+pub use range::*;
 
 pub trait Collate {
     type Value;
 
-    fn bisect_left<V: Deref<Target = [Self::Value]>>(&self, slice: &[V], key: &[Self::Value]) -> usize {
+    fn bisect<V: Deref<Target = [Self::Value]>, B: Borrow<[Self::Value]>>(
+        &self,
+        slice: &[V],
+        range: &Range<Self::Value, B>,
+    ) -> (usize, usize) {
+        debug_assert!(self.is_sorted(slice));
+
+        let left = bisect_left(slice, |key| self.compare_range(key, range));
+        let right = bisect_right(slice, |key| self.compare_range(key, range));
+        (left, right)
+    }
+
+    fn bisect_left<V: Deref<Target = [Self::Value]>>(
+        &self,
+        slice: &[V],
+        key: &[Self::Value],
+    ) -> usize {
+        debug_assert!(self.is_sorted(slice));
+
         if slice.is_empty() || key.is_empty() {
             0
         } else {
@@ -13,7 +36,13 @@ pub trait Collate {
         }
     }
 
-    fn bisect_right<V: Deref<Target = [Self::Value]>>(&self, slice: &[V], key: &[Self::Value]) -> usize {
+    fn bisect_right<V: Deref<Target = [Self::Value]>>(
+        &self,
+        slice: &[V],
+        key: &[Self::Value],
+    ) -> usize {
+        debug_assert!(self.is_sorted(slice));
+
         if slice.is_empty() {
             0
         } else if key.is_empty() {
@@ -24,6 +53,48 @@ pub trait Collate {
     }
 
     fn compare(&self, left: &Self::Value, right: &Self::Value) -> Ordering;
+
+    fn compare_range<B: Borrow<[Self::Value]>>(
+        &self,
+        key: &[Self::Value],
+        range: &Range<Self::Value, B>,
+    ) -> Ordering {
+        use Bound::*;
+        use Ordering::*;
+
+        let prefix_rel = self.compare_slice(key, range.prefix());
+        if prefix_rel != Equal || key.len() < range.len() {
+            return prefix_rel;
+        }
+
+        let target = &key[range.prefix().len()];
+
+        match range.start() {
+            Unbounded => {}
+            Included(value) => match self.compare(target, value) {
+                Less => return Less,
+                _ => {}
+            },
+            Excluded(value) => match self.compare(target, value) {
+                Less | Equal => return Less,
+                _ => {}
+            },
+        }
+
+        match range.end() {
+            Unbounded => {}
+            Included(value) => match self.compare(target, value) {
+                Greater => return Greater,
+                _ => {}
+            },
+            Excluded(value) => match self.compare(target, value) {
+                Greater | Equal => return Greater,
+                _ => {}
+            },
+        }
+
+        Equal
+    }
 
     fn compare_slice<L: Deref<Target = [Self::Value]>, R: Deref<Target = [Self::Value]>>(
         &self,
@@ -53,7 +124,7 @@ pub trait Collate {
             return true;
         }
 
-        let order = self.compare_slice(slice[1].deref(),slice[0].deref());
+        let order = self.compare_slice(slice[1].deref(), slice[0].deref());
         for i in 1..slice.len() {
             let rel = self.compare_slice(slice[i].deref(), slice[i - 1].deref());
             if rel != order && rel != Ordering::Equal {
@@ -120,11 +191,11 @@ fn bisect_right<'a, V: 'a, W: Deref<Target = [V]>, F: Fn(&'a [V]) -> Ordering>(
 
 #[cfg(test)]
 mod tests {
-    use std::ops::Deref;
     use super::*;
+    use std::ops::Deref;
 
     struct Key {
-        inner: Vec<i32>
+        inner: Vec<i32>,
     }
 
     impl Deref for Key {
@@ -136,7 +207,7 @@ mod tests {
     }
 
     struct Block {
-        keys: Vec<Key>
+        keys: Vec<Key>,
     }
 
     impl Deref for Block {
@@ -151,13 +222,23 @@ mod tests {
     fn test_bisect() {
         let block = Block {
             keys: vec![
-                Key { inner: vec![0, -1, 1] },
-                Key { inner: vec![1, 0, 2, 2] },
-                Key { inner: vec![1, 1, 0] },
-                Key { inner: vec![1, 1, 0] },
-                Key { inner: vec![2, 0, -1] },
+                Key {
+                    inner: vec![0, -1, 1],
+                },
+                Key {
+                    inner: vec![1, 0, 2, 2],
+                },
+                Key {
+                    inner: vec![1, 1, 0],
+                },
+                Key {
+                    inner: vec![1, 1, 0],
+                },
+                Key {
+                    inner: vec![2, 0, -1],
+                },
                 Key { inner: vec![2, 1] },
-            ]
+            ],
         };
 
         let collator = Collator::<i32>::default();
@@ -169,6 +250,9 @@ mod tests {
         assert_eq!(collator.bisect_left(&block, &[0]), 0);
         assert_eq!(collator.bisect_right(&block, &[0]), 1);
 
+        assert_eq!(collator.bisect_left(&block, &[1]), 1);
+        assert_eq!(collator.bisect_right(&block, &[1]), 4);
+
         assert_eq!(collator.bisect_left(&block, &[1, 1, 0]), 2);
         assert_eq!(collator.bisect_right(&block, &[1, 1, 0]), 4);
 
@@ -178,5 +262,54 @@ mod tests {
 
         assert_eq!(collator.bisect_left(&block, &[3]), 6);
         assert_eq!(collator.bisect_right(&block, &[3]), 6);
+    }
+
+    #[test]
+    fn test_range() {
+        let block = Block {
+            keys: vec![
+                Key {
+                    inner: vec![0, -1, 1],
+                },
+                Key {
+                    inner: vec![1, -1, 2],
+                },
+                Key {
+                    inner: vec![1, 0, -1],
+                },
+                Key {
+                    inner: vec![1, 0, 2, 2],
+                },
+                Key {
+                    inner: vec![1, 1, 0],
+                },
+                Key {
+                    inner: vec![1, 1, 0],
+                },
+                Key {
+                    inner: vec![1, 2, 1],
+                },
+                Key { inner: vec![2, 1] },
+                Key { inner: vec![3, 1] },
+            ],
+        };
+
+        let collator = Collator::<i32>::default();
+        assert!(collator.is_sorted(&block));
+
+        let range = Range::from(([], 0..1));
+        assert_eq!(collator.bisect(&block, &range), (0, 0));
+
+        let range = Range::from(([1], 0..1));
+        assert_eq!(collator.bisect(&block, &range), (2, 4));
+
+        let range = Range::from(([1], -1..));
+        assert_eq!(collator.bisect(&block, &range), (1, 7));
+
+        let range = Range::from(([1], 0..));
+        assert_eq!(collator.bisect(&block, &range), (2, 7));
+
+        let range = Range::from(([1, 0], ..1));
+        assert_eq!(collator.bisect(&block, &range), (2, 3));
     }
 }
---
+++ src/range.rs
@@ -0,0 +1,69 @@
+use std::borrow::Borrow;
+use std::ops::{self, Bound};
+
+pub struct Range<V, B: Borrow<[V]>> {
+    prefix: B,
+    start: Bound<V>,
+    end: Bound<V>,
+}
+
+impl<V, B: Borrow<[V]>> Range<V, B> {
+    pub fn len(&self) -> usize {
+        self.prefix().len() + 1
+    }
+
+    pub fn prefix(&'_ self) -> &'_ [V] {
+        self.prefix.borrow()
+    }
+
+    pub fn start(&'_ self) -> &'_ Bound<V> {
+        &self.start
+    }
+
+    pub fn end(&'_ self) -> &'_ Bound<V> {
+        &self.end
+    }
+}
+
+impl<V, B: Borrow<[V]>> From<(B, ops::Range<V>)> for Range<V, B> {
+    fn from(tuple: (B, ops::Range<V>)) -> Self {
+        let (prefix, suffix) = tuple;
+        let ops::Range { start, end } = suffix;
+        Self {
+            prefix,
+            start: Bound::Included(start),
+            end: Bound::Excluded(end),
+        }
+    }
+}
+
+impl<V, B: Borrow<[V]>> From<(B, ops::RangeFrom<V>)> for Range<V, B> {
+    fn from(tuple: (B, ops::RangeFrom<V>)) -> Self {
+        let (prefix, suffix) = tuple;
+        let ops::RangeFrom { start } = suffix;
+        Self {
+            prefix,
+            start: Bound::Included(start),
+            end: Bound::Unbounded,
+        }
+    }
+}
+
+impl<V, B: Borrow<[V]>> From<(B, ops::RangeTo<V>)> for Range<V, B> {
+    fn from(tuple: (B, ops::RangeTo<V>)) -> Self {
+        let (prefix, suffix) = tuple;
+        let ops::RangeTo { end } = suffix;
+        Self {
+            prefix,
+            start: Bound::Unbounded,
+            end: Bound::Excluded(end),
+        }
+    }
+}
+
+impl<V, B: Borrow<[V]>> From<(B, ops::Bound<V>, ops::Bound<V>)> for Range<V, B> {
+    fn from(tuple: (B, ops::Bound<V>, ops::Bound<V>)) -> Self {
+        let (prefix, start, end) = tuple;
+        Self { prefix, start, end }
+    }
+}
